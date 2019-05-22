862a6d3473e43c6d4608467760e5f4c8091b6d0c
Skylar Valerio <SJValerio@student.fullsail.com>
2019-05-22 13:28:51 -0400

    Fixed the errors for user_prompting and added the proper .html file

--- Javascript Redone/user_prompting/user_prompting.js
+++ Javascript Redone/user_prompting/user_prompting.js
@@ -1,4 +1,5 @@
 
+
 /* Skylar Valerio
 * 05.14.19
 * C# to Javascript - User Prompting
@@ -13,10 +14,10 @@ let userName = prompt("Hello! What is your name?");
 // validate the username
 while (userName === ""){
     userName = prompt("Please don't leave this blank!\nWhat is your name?");
-    
+
 }
 
-alert("Welcome to the program " + userName + "!");
+alert("\nWelcome to the program " + userName + "!");
 
 
 
@@ -27,7 +28,11 @@ alert("\nWe are going to calculate the area of a rectangle based on your measure
 
 // prompt and validate the rectangle dimensions
 let widthString = Number(prompt("\nWhat is the width of your rectangle?"));
-let heightString = Number(prompt("\nGot it!\nYou typed in a width of "+ widthString +".\nNow, please type in the height of your rectangle."));
+let heightString = Number(prompt("\nGot it!\nYou typed in a width of " + widthString + "." +
+                          "\nNow, please type in the height of your rectangle."));
+
+
+console.log("Rectangle height = " + heightString + " and the width is " + widthString);
 
 // parse the inputs
 let widthInt = parseInt(widthString, 10);
@@ -37,3 +42,6 @@ let heightInt = parseInt(heightString, 10);
 let area = widthInt * heightInt;
 
 alert("\nThe area of the rectangle is "+ area +"!\n\nThank you for using the rectangle calculator!");
+
+console.log("The area of the rectangle is " + area);
+console.log("\nEnd of program.");
