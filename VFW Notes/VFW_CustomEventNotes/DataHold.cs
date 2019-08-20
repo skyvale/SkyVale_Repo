afd3df377cf9363851a5cf6a715e491267fd7617
Skylar <SJValerio@student.fullsail.edu>
2019-08-19 23:37:12 -0400

    Added the notes for Custom Events to the VFW class notes folder

---
+++ VFW Notes/VFW_CustomEventNotes/DataHold.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VFW_CustomEventNotes
+{
+
+    // make the class public so it is available to all forms
+    public class DataHold
+    {
+        // age is a string in this example only because this is a speedy demonstration (it should be decimal)
+        public string firstName;
+        public string lastName;
+        public string age;
+
+    }
+}
