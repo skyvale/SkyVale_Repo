4869504d3faf0d520cc495e30e9fee5d9a617106
Skylar Valerio <skylarvalerio@skylars-mbp.students.fullsail.com>
2019-11-05 13:16:50 -0500

    edited the first assignments code in class to make it better, everything works

--- Portfolio III/Assignments/newsBasic/basic/js/blog.js
+++ Portfolio III/Assignments/newsBasic/basic/js/blog.js
@@ -1,44 +1,65 @@
-document.querySelector("#blog h2").innerHTML = "Pokemon Starters";
-
 // use Ajax to load the JSON file
 var xhr = new XMLHttpRequest();
 xhr.onload = function(){
     
     // save JSON data
-    let data = JSON.parse(xhr.responseText);
+   let data = JSON.parse(xhr.responseText);
 
-    // insert the HTML articles
+   // insert the HTML articles
+   // first check to make sure the blog exists
+   let blogSection = document.querySelector("#blog");
+   if (blogSection){
 
-   let htmlTemplate = "";
-   for (let i = 0; i < data.starters.length; i++){    
-    starterMon = `
-       <article>
-           <p class="thumbnail">
-           <img src="${data.starters[i].image}" alt="${data.starters[i].name}"></img></p>
-           
-           <h3>${data.starters[i].name}</h3>
-           <p>${data.starters[i].pokedex}.</p>
-           
-           <dl>
-               <dt>Type</dt>
-               <dd>${data.starters[i].type}</dd>
-               <dt>Ability</dt>
-               <dd>${data.starters[i].ability}</dd>
-               <dt>Base Stats</dt>
-               <dd>${data.starters[i].base_stats}</dd>					
-           </dl>
-           
-           <p><a href="${data.starters[i].site_link}">Read More</a></p>
-       </article>
+    // edit the heading
+    let blogHeading = blogSection.querySelector("h2");
+    if (blogHeading) {
+        let heading  = 'Pokemon Starters';
+        blogHeading.innerHTML = heading;
+    }
+    
+    // edit the seperate articles by looping through the JSON file
+    let htmlTemplate = "";
+    for (let i = 0; i < data.starters.length; i++){    
+     htmlTemplate += `
+        <article>
+            <p class="thumbnail">
+            <img src="${data.starters[i].image}" alt="${data.starters[i].name}"></img></p>
+            
+            <h3>${data.starters[i].name}</h3>
+            <p>${data.starters[i].pokedex}.</p>
+            
+            <dl>
+                <dt>Type</dt>
+                <dd>${data.starters[i].type}</dd>
+                <dt>Ability</dt>
+                <dd>${data.starters[i].ability}</dd>
+                <dt>Base Stats</dt>
+                <dd>${data.starters[i].base_stats}</dd>					
+            </dl>
+            
+            <p><a href="${data.starters[i].site_link}">Read More</a></p>
+        </article>
+ 
+        `
+    
+    }
 
-       `
+    // insertAdjacent is to ADD content whereas 
+    // innerHTML is to REPLACE content within the HTML file
+    blogHeading.insertAdjacentHTML('afterend', htmlTemplate);
 
-       htmlTemplate += starterMon;
+
+    let otherGenButton = blogSection.querySelector('#blog > p');
+    console.log(otherGenButton);
+    if (otherGenButton){
+        let buttonText = "<a href='https://www.serebii.net/pokemon/all.shtml'>Other Generations</button></a>";
+        otherGenButton.innerHTML = buttonText;
+        
+    }
+    
+     
    }
-   
-   // let blogSection = document.querySelector("#blog");
-   document.getElementById("blog").innerHTML = "<h2>Pokemon Starters</h2>"+htmlTemplate+"<p><a href='https://www.serebii.net/pokemon/all.shtml'><button>Other Generations</button></a></p>";
-   
+
 }
 
 // call + close Ajax
